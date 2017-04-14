1cea74c4075fbf5ab74f228d8e2453bfc7d57457
Ricardo <rick@reddraft.com>
2017-04-14 11:11:58 -0500

    Added animations on header

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+
+  /******************************
+  ***       ANIMATIONS         **
+  *******************************/
+
+  // header animations
+  $('.js-wp-animate-logo').addClass('animated fadeInDown');
+  $('.js-wp-animate-main-heading').addClass('animated fadeInDown');
+  $('.js-wp-animate-hero-text').addClass('animated fadeInDown');
+  $('.js-wp-animate-form').addClass('animated fadeInDown');
+  $('.js-wp-animate-hero-h5').addClass('animated fadeInDown');
+  $('.js-wp-animate-scroll').addClass('animated fadeInUp');
+});
