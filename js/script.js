741163dec5b3346bcf93ebe167d705755f8d314c
Ricardo <rick@reddraft.com>
2017-04-14 13:02:58 -0500

    Add animation to ebbok

--- js/script.js
+++ js/script.js
@@ -22,14 +22,48 @@ $(document).ready(function() {
   });
 
   /******************************
-  ***       ANIMATIONS         **
+  ***    HEADER ANIMATIONS     **
   *******************************/
 
   // header animations
+  $('.js-wp-animate-ebook').addClass('animated fadeInLeft');
   $('.js-wp-animate-logo').addClass('animated fadeInDown');
   $('.js-wp-animate-main-heading').addClass('animated fadeInDown');
   $('.js-wp-animate-hero-text').addClass('animated fadeInDown');
   $('.js-wp-animate-form').addClass('animated fadeInDown');
   $('.js-wp-animate-hero-h5').addClass('animated fadeInDown');
   $('.js-wp-animate-scroll').addClass('animated fadeInUp');
+});
+
+/******************************
+*** INTRO SECTION ANIMATIONS **
+*******************************/
+
+$('.intro-section').waypoint(function() {
+    $('.js-wp-animate-intro-heading').addClass('animated fadeInDown');
+    $('.js-wp-animate-intro-text').addClass('animated fadeInUp');
+
+}, {
+  offset: '400px;'
+});
+
+
+
+
+
+
+/******************************
+***    FOOTER ANIMATIONS     **
+*******************************/
+var inview = new Waypoint.Inview({
+element: $('html, body')[0],
+
+  entered: function(direction) {
+    if (direction === 'down') {
+      $('.js-wp-animate-secondary-logo').addClass('animated fadeInDown');
+      $('.js-wp-animate-social-icons').addClass('animated fadeInUp');
+      $('.js-wp-animate-small').addClass('animated fadeInUp');
+    }
+  }
+
 });
